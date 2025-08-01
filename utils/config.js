3cb0c8c88dad36cab01e25f1926d1aff84702089
git <laghomklaus@yahoo.fr>
2025-08-01 05:20:32 +0100

    Add env var for JWT secret

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,7 @@
-const JWT_SECRET = "very-secure-secret-key";
+// const JWT_SECRET = "very-secure-secret-key";
+
+// module.exports = { JWT_SECRET };
+// config.js
+const { JWT_SECRET = "super-strong-secret" } = process.env;
 
 module.exports = { JWT_SECRET };
