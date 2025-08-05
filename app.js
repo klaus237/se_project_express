8b827072b363073e1362ecfa624e362a64044e05
git <laghomklaus@yahoo.fr>
2025-08-05 05:29:15 +0100

    Ajout crash-test et autres modifs

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 // Utiliser le routeur principal
 app.use("/", mainRouter);
 app.use(errorLogger);
