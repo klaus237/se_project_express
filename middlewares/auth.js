ad2af657d474a7daf0785c1372f41b3bfa72cef4
git <laghomklaus@yahoo.fr>
2025-08-07 22:40:15 +0100

    fix: 'UNAUTHORIZED' is assigned a value but never used in auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return next(new UnauthorizedError("Authorization required"));
+    return next(new UnauthorizedError(UNAUTHORIZED));
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -17,6 +17,6 @@ module.exports = (req, res, next) => {
     req.user = payload;
     return next();
   } catch (err) {
-    return next(new UnauthorizedError("Invalid or expired token"));
+    return next(new UnauthorizedError(UNAUTHORIZED));
   }
 };
